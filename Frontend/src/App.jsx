b8176d4eb13944a53bbfe23edf40cbd2c919d79f
mjasir-10 <jasirmm307@gmail.com>
2024-07-14 11:12:26 +0600

    navbar added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
-import Aboutus from './pages/Aboutus';
+import About from './pages/About';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 
 const App = () => {
-  return (
-    <BrowserRouter>
-    <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about-us' element={<Aboutus />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/project' element={<Projects />} />
-    </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about-us' element={<About />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/sign-up' element={<Signup />} />
+                <Route path='/dashboard' element={<Dashboard />} />
+                <Route path='/projects' element={<Projects />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,76 @@
+import { Button, Navbar, NavbarCollapse, NavbarLink, NavbarToggle, TextInput } from 'flowbite-react'
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { IoSearchOutline } from "react-icons/io5";
+import { FaMoon } from "react-icons/fa";
+
+const Header = () => {
+    const path = useLocation().pathname;
+
+    return (
+        <header>
+            <Navbar className='bg-slate-100 flex items-center justify-between w-full sm:px-10 px-2 py-2 z-10'>
+                <Link to='/' className="flex items-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+                    <h2 className=''>
+                        <span className='bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-3xl px-2 py-1 pb-2 text-white'>The Blog </span> Spot
+                    </h2>
+                </Link>
+
+                <div className='flex'>
+                    <TextInput
+                        type='text'
+                        placeholder='Search...'
+                        rightIcon={IoSearchOutline}
+                        className='hidden lg:flex '
+                    />
+                </div>
+
+                <div className='flex gap-4 items-center md:order-2'>
+
+                    <Button
+                        className='w-12 h-12 lg:hidden'
+                        color='gray'
+                        pill
+                    >
+                        <IoSearchOutline size='23' color="black" />
+                    </Button>
+
+                    <Button
+                        className='w-12 h-12 max-sm:hidden'
+                        color='gray'
+                        pill
+                    >
+                        <FaMoon size='23' />
+                    </Button>
+
+                    <Link to='/login' className='flex'>
+                        <Button
+                            gradientDuoTone="greenToBlue"
+                            outline
+                        >
+                            Login
+                        </Button>'
+                    </Link>
+                    <NavbarToggle />
+                </div>
+
+                <NavbarCollapse>
+                    <NavbarLink as={Link} to="/" active={path === '/'}>
+                        Home
+                    </NavbarLink>
+                    <NavbarLink as={Link} to="/about" active={path === '/about'}>
+                        About
+                    </NavbarLink>
+                    <NavbarLink as={Link} to="/projects" active={path === '/projects'}>
+                        Projects
+                    </NavbarLink>
+                    <NavbarLink as={Link} to="/contact" active={path === '/contact'}>
+                        Contact
+                    </NavbarLink>
+                </NavbarCollapse>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
