c186d8ca405c9aef0cfcb488210f6c89f666c061
mjasir-10 <jasirmm307@gmail.com>
2024-07-14 13:20:28 +0600

    mongodb connection added and user model added

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/user-route.js')
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() => console.log('MongoDB is connected !!'))
+.catch((error) => console.log(error));
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+app.use('/api/user', userRoute);
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+});
+
