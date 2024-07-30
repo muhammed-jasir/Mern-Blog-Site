7cf1167650f92760d88086eeb1f5b20942706f51
muhammed-jasir <jasirmm307@gmail.com>
2024-07-30 14:44:48 +0600

    get users api

--- Backend/routes/user-route.js
+++ Backend/routes/user-route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const verifyToken = require('../utils/verifyUser');
-const {updateProfile, deleteUser, signout } = require('../controllers/user-controller');
+const {updateProfile, deleteUser, signout, getUsers } = require('../controllers/user-controller');
 const router = express.Router();
 
 router.put('/update/:userId', verifyToken, updateProfile);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', verifyToken, signout);
+router.get('/get-users', verifyToken, getUsers);
 
 module.exports = router;
